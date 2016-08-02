1c1908e6158e62afc24dc652b308642f30288edc
wac81 <wuanch@gmail.com>
2016-08-02 14:44:56 +0800

    add httpserver

--- http/httpserver.py
+++ http/httpserver.py
@@ -1,5 +1,8 @@
 # -*- coding:utf-8 -*-
 # coding=utf-8
+# from gevent import monkey
+# monkey.patch_all()
+
 import logging
 logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
 
@@ -8,7 +11,6 @@ import json
 from flask import Flask, request, abort,g,current_app
 # from werkzeug.contrib.fixers import ProxyFix
 app = Flask(__name__)
-
 
 @app.route('/similar/<input_text>',methods=['GET', 'POST'])
 def similar(input_text):
@@ -28,3 +30,4 @@ if __name__ == '__main__':
     with app.app_context():
         print current_app.name
     app.run(debug=False, host='0.0.0.0', port=3001)
+    # app.run()
