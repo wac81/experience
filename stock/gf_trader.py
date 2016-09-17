4c20ae9c31b93cd7bf40dea02d57f9e5814defb3
wac <wuanch@gmail.com>
2016-09-17 13:51:28 +0800

    gf_trader

--- stock/gf_trader.py
+++ stock/gf_trader.py
@@ -1,6 +1,13 @@
 #coding=utf8
 '''
 广发证券必须安装pytesseract，用于验证
+a、Python-tesseract支持python2.5及更高版本；  sudo pip install pytesseract
+
+b、Python-tesseract需要安装PIL（Python Imaging Library） ，来支持更多的图片格式；sudo apt-get install python-imaging
+mac:sudo port install tesseract  or brew install tesseract
+
+c、Python-tesseract需要安装tesseract-ocr安装包。sudo apt-get install tesseract-ocr
+
 '''
 import easytrader
 import os
