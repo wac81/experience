0481bcb7c683236c766f2f9e1d70c9144780f27a
wac <wuanch@gmail.com>
2016-11-29 00:56:46 +0800

    lstm

--- opencv/FaceDetection/Face_Detection.py
+++ opencv/FaceDetection/Face_Detection.py
@@ -7,29 +7,36 @@ cascPath = "haarcascade_frontalface_alt.xml"
 faceCascade = cv2.CascadeClassifier(cascPath)
 
 video_capture = cv2.VideoCapture(0)
-video_capture.set(3, 640)
-video_capture.set(4, 480)
-
+video_capture.set(3, 1920)
+video_capture.set(4, 1080)
+SKIP_FRAMES = 3
+dets = []
+count = 0
+face_fx = face_fy = 0.33
 while True:
     # Capture frame-by-frame
     ret, frame = video_capture.read()
 
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-    faces = faceCascade.detectMultiScale(
-        gray,
-        scaleFactor=1.06,
-        minNeighbors=8,
-        # minSize=(30, 30),
-
-        # flags=cv2.cv.CV_HAAR_SCALE_IMAGE
-    )
 
     # Draw a rectangle around the faces
+
+
+    if count % SKIP_FRAMES == 0:
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        small = cv2.resize(frame, (0, 0), fx=face_fx, fy=face_fx)
+
+
+        faces = faceCascade.detectMultiScale(
+            small,
+            scaleFactor=1.06,
+            minNeighbors=8,
+            # minSize=(30, 30),
+
+            # flags=cv2.cv.CV_HAAR_SCALE_IMAGE
+        )
     for (x, y, w, h) in faces:
-        cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
-
-
+        cv2.rectangle(frame, (int(x/face_fx), int(y/face_fy)), (int((x + w)/face_fx), int((y + h)/face_fy)), (0, 255, 0), 2)
     # Display the resulting frame
     cv2.imshow('Video', frame)
 
--- opencv/FaceDetection/face_detection_camera_dlib.py
+++ opencv/FaceDetection/face_detection_camera_dlib.py
@@ -8,15 +8,24 @@ cascPath = "haarcascade_frontalface_alt.xml"
 faceCascade = cv2.CascadeClassifier(cascPath)
 
 video_capture = cv2.VideoCapture(0)
-video_capture.set(3, 640)
-video_capture.set(4, 480)
+video_capture1 = cv2.VideoCapture(1)
+# video_capture.set(3, 640)
+# video_capture.set(4, 480)
+video_capture.set(3, 1280)
+video_capture.set(4, 720)
+font = cv2.FONT_HERSHEY_SIMPLEX
+SKIP_FRAMES = 3
+dets = []
+count = 0
+face_fx = face_fy = 0.33
 
 while True:
     # Capture frame-by-frame
     ret, frame = video_capture.read()
+    ret1, frame1 = video_capture1.read()
+    cv2.imshow('Video2', frame1)
 
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    dets = detector(gray, 1)
+
     # faces = faceCascade.detectMultiScale(
     #     gray,
     #     scaleFactor=1.06,
@@ -28,8 +37,12 @@ while True:
 
     # Draw a rectangle around the faces
     # for (x, y, w, h) in faces:
+    if count % SKIP_FRAMES == 0:
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        small = cv2.resize(frame, (0, 0), fx=face_fx, fy=face_fx)
+        dets = detector(small, 1)
     for i, d in enumerate(dets):
-        cv2.rectangle(frame, (d.left(), d.top()), (d.right(), d.bottom()), (0, 255, 0), 2)
+        cv2.rectangle(frame, (int(d.left()/face_fx), int(d.top()/face_fy)), (int(d.right()/face_fx), int(d.bottom()/face_fy)), (0, 255, 0), 2)
 
 
     # Display the resulting frame
