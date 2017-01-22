f8e521fe9be6212ef7fc42a2a22b5f01bde48d5f
wac <wuanch@gmail.com>
2017-01-22 15:21:46 +0800

    pig_latin

--- keras/TimeDistribute.py
+++ keras/TimeDistribute.py
@@ -9,7 +9,7 @@ import random
 
 # as the first layer in a model
 model = Sequential()
-model.add(TimeDistributed(Dense(8), input_shape=(10, 16)))
+model.add(TimeDistributed(Dense(8), input_shape=(10, 3)))
 # now model.output_shape == (None, 10, 8)
 
 # subsequent layers: no need for input_shape
@@ -20,14 +20,19 @@ model.add(Activation("linear"))
 model.compile(loss="mean_squared_error", optimizer="rmsprop")
 
 #prepare dataset
-X_train = np.random.random((100, 10, 16))
+X_train = np.random.random((100, 10, 3))
 print ('random array:', X_train)
 # y_train = \
 mu,sigma = 0,1 #均值与标准差
 y_train = []
-for i in range(100):
+y_train_temp = []
+for i in range(10):
     single = np.random.choice([0,1])
-    y_train.append([single, 1-single])
+    y_train_temp.append([single, 1-single])
+for i in range(100):
+    # single = np.random.choice([0,1])
+    y_train.append(y_train_temp)
+
 # y_train = np.random.normal(mu,sigma,10)
 print ('random array:', y_train)
 
