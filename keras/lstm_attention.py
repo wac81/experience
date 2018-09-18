a47f88ea77e8d299f4764e2e5df25944ffd2ab84
wac <wuanch@gmail.com>
2018-09-18 14:32:14 +0800

    add grpc

--- keras/lstm_attention.py
+++ keras/lstm_attention.py
@@ -22,15 +22,34 @@ import codecs
 # text = open(path).read().lower()
 from keras.models import Model
 from keras.layers import *
+import jieba
+import os
+from backports import csv
 
+# text = codecs.open(u"星河大帝.txt", encoding='utf8').read()
 
-text = codecs.open(u"唐诗三百首.txt", encoding='utf8').read()
+data_path = './weixin'
+text = ''
+for path in os.listdir(data_path):
+    with codecs.open(os.path.join(data_path, path), encoding='utf8') as csvfile:
+        spamreader = csvfile.readlines()[1:]
+        for row in spamreader:
+            row_list = row.split(',')
+            if len(row_list) >= 10:
+                text += row_list[9]
+
 print('corpus length:', len(text))
 
 chars = set(text)
 print('total chars:', len(chars))
 char_indices = dict((c, i) for i, c in enumerate(chars))
 indices_char = dict((i, c) for i, c in enumerate(chars))
+
+words = set(jieba.lcut(text))
+print('total words:', len(words))
+word_indices = dict((c, i) for i, c in enumerate(words))
+indices_word = dict((i, c) for i, c in enumerate(words))
+
 
 # cut the text in semi-redundant sequences of maxlen characters
 maxlen = 8
@@ -44,23 +63,42 @@ print('nb sequences:', len(sentences))
 
 print('Vectorization...')
 X = np.zeros((len(sentences), maxlen), dtype=np.int16)
+
+X_W = np.zeros((len(sentences), maxlen), dtype=np.int16)
+
 y = np.zeros((len(sentences), len(chars)), dtype=np.bool)
 for i, sentence in enumerate(sentences):
     for t, char in enumerate(sentence):
         X[i, t] = char_indices[char]
+    for t, word in enumerate(jieba.lcut(sentence)):
+        try:
+            X_W[i, t] = word_indices[word]
+        except KeyError:
+            continue
+
     y[i, char_indices[next_chars[i]]] = 1
 
 
 # build the model: 2 stacked LSTM
 print('Build model...')
 # model = Sequential()
-S_inputs = Input(shape=(None, ), dtype='int16')
-embeddings = Embedding(len(chars), maxlen)(S_inputs)
+char_inputs = Input(shape=(None, ), dtype='int16')
+word_inputs = Input(shape=(None, ), dtype='int16')
+
+embeddings = Embedding(len(chars), maxlen)(char_inputs)
+W_embeddings = Embedding(len(words), maxlen)(word_inputs)
+
 # model.add(LSTM(1024, return_sequences=True, input_shape=(maxlen, len(chars))))
-lstm = LSTM(1024, return_sequences=True)(embeddings)
-O_seq = Attention(8, 16)([lstm, embeddings, embeddings])
-O_seq = Dropout(0.2)(lstm)
-O_seq = LSTM(1024, return_sequences=False)(O_seq)
+lstm = Bidirectional(LSTM(512, return_sequences=True, recurrent_dropout=0.2))(embeddings)
+W_lstm = Bidirectional(LSTM(512, return_sequences=True, recurrent_dropout=0.2))(W_embeddings)
+add_layer = merge([lstm, W_lstm], output_shape=512, mode='sum')
+O_seq = Dropout(0.4)(add_layer)
+O_seq = Bidirectional(LSTM(512, return_sequences=False))(O_seq)
+
+# attention_probs = Dense(512, activation='softmax', name='attention_probs')(Flatten()(embeddings))
+# attention_mul = merge([O_seq, attention_probs], output_shape=512, name='attention_mul', mode='mul')
+
+O_seq = Dropout(0.4)(O_seq)
 O_seq = Dense(len(chars))(O_seq)
 outputs = Activation('softmax')(O_seq)
 # model.add(Dropout(0.2))
@@ -68,10 +106,13 @@ outputs = Activation('softmax')(O_seq)
 # model.add(Dropout(0.2))
 # model.add(Dense(len(chars)))
 # model.add(Activation('softmax'))
-model = Model(inputs=S_inputs, outputs=outputs)
+model = Model(inputs=[char_inputs, word_inputs], outputs=outputs)
 optimizer = RMSprop(lr=0.01)
 
-model.compile(loss='categorical_crossentropy', optimizer=optimizer)
+model.compile(loss='categorical_crossentropy',
+                optimizer='nadam',
+              # optimizer=optimizer
+              )
 
 
 def sample(preds, temperature=1.0):
@@ -88,11 +129,11 @@ for iteration in range(1, 100):
     print()
     print('-' * 50)
     print('Iteration', iteration)
-    model.fit(X, y, batch_size=32, nb_epoch=1)
+    model.fit([X, X_W], y, batch_size=4096, nb_epoch=1)
 
     start_index = random.randint(0, len(text) - maxlen - 1)
 
-    for diversity in [0.2, 0.5, 0.8, 1.0, 1.2]:
+    for diversity in [0.5, 0.8, 1.0, 1.2]:
         print()
         print('----- diversity:', diversity)
 
@@ -102,12 +143,24 @@ for iteration in range(1, 100):
         print('----- Generating with seed: "' + sentence + '"')
         sys.stdout.write(generated)
 
-        for i in range(100):
-            x = np.zeros((1, maxlen, len(chars)))
-            for t, char in enumerate(sentence):
-                x[0, t, char_indices[char]] = 1.
+        for i in range(200):
+            # x = np.zeros((1, maxlen, len(chars)))
 
-            preds = model.predict(x[0], verbose=0)[0]
+            x = np.zeros((1, maxlen))
+            x_w = np.zeros((1, maxlen))
+
+            for t, char in enumerate(sentence):
+                # x[0, t, char_indices[char]] = 1.
+                x[0, t] = char_indices[char]
+
+            for t, word in enumerate(jieba.lcut(sentence)):
+                # x[0, t, char_indices[char]] = 1.
+                try:
+                    x_w[0, t] = word_indices[word]
+                except KeyError:
+                    continue
+
+            preds = model.predict([x,x_w], verbose=0)[0]
             next_index = sample(preds, diversity)
             next_char = indices_char[next_index]
 
